292dd5ab9cda4d9630b5a13e67c2a6ee94ff365b
Mohammed Arefeen <mohammed.arefeen@mnp.ca>
2021-10-29 14:54:45 -0400

    weather lookup by city

--- components/agility-pageModules/WeatherSearch.js
+++ components/agility-pageModules/WeatherSearch.js
@@ -6,7 +6,7 @@ const WeatherSearch = ({ module }) => {
   const [query, setQuery] = useState("");
   const [active, setactive] = useState(false);
   const [results, setResults] = useState([]);
-  const [selectedResult, setSelectedResult] = useState(null)
+  const [cityResults, setCityResults] = useState(null);
 
   const router = useRouter();
   const { lang } = router.query;
@@ -18,7 +18,7 @@ const WeatherSearch = ({ module }) => {
     const query = e.target.value;
     setQuery(query);
     if (query.length > 2) {
-     await fetch(`/api/weather?query=${query}`)
+      await fetch(`/api/weather?query=${query}`)
         .then((res) => res.json())
         .then((res) => {
           console.log(res, "res");
@@ -29,41 +29,65 @@ const WeatherSearch = ({ module }) => {
     }
   }, []);
 
-  const onClickResult = (result) => setSelectedResult(result)
-  const onBlurResult = () => setSelectedResult(null)
+  const onClickResult = async (result) => {
+    setResults([]);
+    console.log("lolz", result);
+    setQuery(result);
+    await fetch(`/api/weather/city?query=${result}`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res, "res");
+        setResults(res);
+        setCityResults(res);
+      });
+  };
+  const onBlurResult = () => setCityResults(null);
 
+  // const onBlurSuggestions = () => {
+  //   console.log('what')
+  //   setResults([])}
 
   // get module fields
   const { fields } = module;
   console.log(fields, module, "fields and module");
   return (
     <div className="relative px-8">
-      <h1>{fields.searchTitle}</h1>
+      <h1 className="font-display text-secondary-500 text-4xl font-black tracking-wide">{fields.searchTitle}</h1>
       <StyledDiv className="max-w-4xl mx-auto my-12 md:mt-18 lg:mt-20">
-        {/* <div
-          className="prose max-w-full mx-auto"
-          dangerouslySetInnerHTML={renderHTML(fields.textblob)}
-        /> */}
         <input
           type="search"
           placeholder={fields.searchPlaceholder}
           onChange={onChange}
           value={query}
           className="prose max-w-full mx-auto"
+          // onBlur={onBlurSuggestions}
         />
-        {results && (
-          <StyledList className="list-disc">
-            {results.length > 0 &&
-              results.map((weather) => (
-                <StyledListItem key={weather.id} onClick={onClickResult}>{weather.name}</StyledListItem>
-              ))}
+        {results && results.length > 0 && (
+          <StyledList>
+            {results.map((weather) => (
+              <StyledListItem
+                key={weather.id}
+                onClick={() => onClickResult(weather.name)}
+              >
+                {weather.name}
+              </StyledListItem>
+            ))}
           </StyledList>
         )}
-        {selectedResult && (
+        {cityResults && (
           <ResultCard onBlur={onBlurResult}>
-              <ResultItem>some detail</ResultItem>
-              <ResultItem>some detail</ResultItem>
-              <ResultItem>some detail</ResultItem>
+            <ResultItem>
+              Temperature: {cityResults.current.temp_c}°C /{" "}
+              {cityResults.current.temp_f}°F
+            </ResultItem>
+            <ResultItem>
+              Feels Like: {cityResults.current.feelslike_c}°C /{" "}
+              {cityResults.current.feelslike_f}°F
+            </ResultItem>
+            <ResultItem className="blocked-result">
+              {cityResults.current.condition.text}
+              <img src={cityResults.current.condition.icon} />
+            </ResultItem>
           </ResultCard>
         )}
       </StyledDiv>
@@ -81,40 +105,50 @@ const StyledDiv = styled.div`
   input[type="search"] {
     font: 1em/1.618 Open Sans, Arial, Sans-serif;
     border: 0.125em solid #737373;
-    border-width: 0 0 3px;
+    border-width: 0 0 1px;
     background-color: transparent;
     padding: 0.1875em 0.375em;
-    width: 80%;
+    width: 100%;
   }
 `;
 
 const StyledList = styled.ul`
-  margin-top: 15px;
+  margin-top: 0px;
   border: 0.125em solid #737373;
   border-width: 1px;
+  padding: 10px 5px;
 
-  // li:hover {
-  //   background: yellow;
-  // }
+  li:hover {
+    background: yellow;
+  }
 `;
 
 const StyledListItem = styled.li`
+  border: 0.125em solid #737373;
+  border-width: 0 0 1px;
   &::hover {
     background: black;
   }
-`
+`;
 const ResultCard = styled.div`
-  margin: 5px;
+  margin-top: 15px;
   display: flex;
   width: 100%;
   align-items: center;
   justify-content: center;
   border: 0.125em solid #737373;
   border-width: 1px;
-`
+  padding: 10px 15px;
+  background: rgba(34, 34, 34);
+  color: #fff;
+`;
 
 const ResultItem = styled.div`
   width: 80%;
   padding: 5px;
-  background: pink;
-`
+  &.blocked-result{
+    display: flex;
+  align-items: center;
+  justify-content: center;
+  }
+`;
--- pages/api/weather/city/index.js
+++ pages/api/weather/city/index.js
@@ -1,11 +1,11 @@
-import {getWeatherData} from "../../../util/weatherApi"
+import {getWeatherByCity} from "../../../../util/weatherApi"
 
 
 export default async (req, res) => {
     const {query} = req.query;
     try {
         console.log('string is',query)
-        const data = await getWeatherData({query})
+        const data = await getWeatherByCity({query})
         res.json(data);
     } catch (e) {
         res.status(400).json({error: 'cannot find data'})
